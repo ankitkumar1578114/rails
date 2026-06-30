a71fc2d5b9c6aa3f47a8ac1f71a40b968510c860
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2026-06-30 23:19:12 +0530

    Some changes

--- api/repos/db.py
+++ api/repos/db.py
@@ -1,31 +1,35 @@
+import os
 from contextlib import contextmanager
-from typing import Generator, Optional
+from typing import Any, Dict, Generator, Optional
 
 import mysql.connector
 from mysql.connector import pooling
 
 _pool: Optional[pooling.MySQLConnectionPool] = None
 
-DB_CONFIG = {
-#   "host": "127.0.0.1",
-#   "user": "root",
-#   "password": "",
-#   "database": "mydb",
-  "host": "bwr2tjeeysysm7um7pfo-mysql.services.clever-cloud.com",
-  "user": "ucg3v1n4o6kbgzk2",
-  "password": "8CJNC9GDRkkpe5kPvzJw",
-  "database": "bwr2tjeeysysm7um7pfo",
-}
+
+def get_db_config() -> Dict[str, Any]:
+    config: Dict[str, Any] = {
+        "host": os.environ.get("DB_HOST", "127.0.0.1"),
+        "user": os.environ.get("DB_USER", "root"),
+        "password": os.environ.get("DB_PASSWORD", ""),
+        "database": os.environ.get("DB_NAME", "mydb"),
+    }
+    port = os.environ.get("DB_PORT")
+    if port:
+        config["port"] = int(port)
+    return config
 
 
 def _get_pool() -> pooling.MySQLConnectionPool:
     global _pool
     if _pool is None:
+        pool_size = int(os.environ.get("DB_POOL_SIZE", "1"))
         _pool = pooling.MySQLConnectionPool(
             pool_name="rails_api_pool",
-            pool_size=1,
+            pool_size=pool_size,
             pool_reset_session=True,
-            **DB_CONFIG,
+            **get_db_config(),
         )
     return _pool
 
